02a36165b6d0de9ca97e1e010611b098fb7aa0e5
euna2000 <eunakoh2000@gmail.com>
2023-07-19 21:30:22 +0900

    API URL 수정

--- src/main/java/com/excelseven/backoffice/controller/PostController.java
+++ src/main/java/com/excelseven/backoffice/controller/PostController.java
@@ -2,6 +2,7 @@ package com.excelseven.backoffice.controller;
 
 import com.excelseven.backoffice.dto.PostRequestDto;
 import com.excelseven.backoffice.dto.PostResponseDto;
+import com.excelseven.backoffice.entity.Post;
 import com.excelseven.backoffice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/posts")
 public class PostController {
 
     private final PostService postService;
@@ -19,27 +19,27 @@ public class PostController {
         this.postService = postService;
     }
 
-    @GetMapping
+    @GetMapping("/api/posts")
     public List<PostResponseDto> getAllPosts() {
         return postService.getAllPosts();
     }
 
-    @GetMapping("/{postId}")
+    @GetMapping("/posts/{postId}")
     public PostResponseDto getPostById(@PathVariable Long postId) {
         return postService.getPostById(postId);
     }
 
-    @PostMapping
+    @PostMapping("/post")
     public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto) {
         return postService.createPost(postRequestDto);
     }
 
-    @PutMapping("/{postId}")
+    @PutMapping("/posts/{postId}")
     public PostResponseDto updatePost(@PathVariable Long postId, @RequestBody PostRequestDto postRequestDto) {
         return postService.updatePost(postId, postRequestDto);
     }
 
-    @DeleteMapping("/{postId}")
+    @DeleteMapping("/posts/{postId}")
     public void deletePost(@PathVariable Long postId) {
         postService.deletePost(postId);
     }
