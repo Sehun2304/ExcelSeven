4b0579fc1b7c5c7724aa562a624aeb508adf2d46
wlstj <redvoca100466@gmail.com>
2023-07-20 14:20:42 +0900

    임시 커밋

--- src/main/java/com/excelseven/backoffice/config/Config.java
+++ src/main/java/com/excelseven/backoffice/config/Config.java
@@ -1,13 +1,25 @@
 package com.excelseven.backoffice.config;
 
+import com.excelseven.backoffice.jwt.JwtAuthorizationFilter;
+import com.excelseven.backoffice.jwt.JwtUtil;
+import com.excelseven.backoffice.security.UserDetailsServicelmpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class Config {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServicelmpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -17,9 +29,20 @@ public class Config {
          *  기본 설정을 사용하지 않고, securityFilterChain 을 재정의함
          *  그리고 모든 요청을 대해 허용으로 바꿈
          */
-        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll()) // 모든 경로 허용
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+//        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll()) // 모든 경로 허용
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
+
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+            return configuration.getAuthenticationManager();
+        }
+
+        @Bean
+        public JwtAuthorizationFilter jwtAuthorizationFilter() {
+            return new JwtAuthorizationFilter(jwtUtil, userDetailsService);
+        }
+
         return http.build();
 
             //특정 경로만 허용할 때 위에 내용 주석하고 아래내용 주석풀기
--- src/main/java/com/excelseven/backoffice/repository/UserRepository.java
+++ src/main/java/com/excelseven/backoffice/repository/UserRepository.java
@@ -8,6 +8,10 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findById(Long id);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByNickname(String nickname);
     //Optional은 값이 있을 수 도 없을 수도 있는 상황에 사용
     //findById는 Jpa를 사용하여 데이터베이스에서 엔티티를 조회하는 메서드
 
