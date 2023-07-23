12e3b76a07efa7a64ffead5e9398c6aa0e858742
wlstj <redvoca100466@gmail.com>
2023-07-23 14:44:34 +0900

    회원가입, 로그인, 로그아웃 Serivce 처리 추가

--- src/main/java/com/excelseven/backoffice/service/UserService.java
+++ src/main/java/com/excelseven/backoffice/service/UserService.java
@@ -1,7 +1,53 @@
 package com.excelseven.backoffice.service;
 
 
+import com.excelseven.backoffice.dto.ApiResponseDto;
+import com.excelseven.backoffice.dto.SigninRequestDto;
+import com.excelseven.backoffice.dto.SignupRequestDto;
+import com.excelseven.backoffice.dto.UserResponseDto;
+import com.excelseven.backoffice.entity.User;
+import com.excelseven.backoffice.jwt.JwtUtil;
+import com.excelseven.backoffice.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
+    public void signup(SignupRequestDto requestDto) {
+
+        String username = requestDto.getUsername();
+//        String nickname = requestDto.getNickname();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        // 회원 중복 확인
+        userRepository.findByUsername(username)
+                .ifPresent(user -> {
+                    throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+                });
+
+        userRepository.save(new User(requestDto, password));
+    }
+
+    public void signin(SigninRequestDto requestDto, HttpServletResponse response) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록되지 않은 사용자입니다.")
+        );
+
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        String token = jwtUtil.createToken(user.getUsername());
+        jwtUtil.addJwtToCookie(token, response);
+    }
 }
