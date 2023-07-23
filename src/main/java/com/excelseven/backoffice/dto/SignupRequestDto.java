ffaa6d7ca64bb82aee207749ebec77e90609c06f
wlstj <redvoca100466@gmail.com>
2023-07-23 15:33:36 +0900

    commit

--- src/main/java/com/excelseven/backoffice/dto/SignupRequestDto.java
+++ src/main/java/com/excelseven/backoffice/dto/SignupRequestDto.java
@@ -1,8 +1,10 @@
 package com.excelseven.backoffice.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +13,7 @@ import lombok.Setter;
 public class SignupRequestDto {
 
     @NotBlank
-    @Size(min = 4, max = 10, message = "4자 이상, 10자 이하")
+    @Size(min = 4, max = 10, message = "4자 이상, 10자 이하로 작성해주세요.")
     @Pattern(regexp = "^[a-z0-9]*$", message = "알파벳 소문자(a~z), 숫자(0~9)")
     private String username;
 
@@ -21,7 +23,16 @@ public class SignupRequestDto {
 //    private String nickname;
 
     @NotBlank
-    @Size(min = 8, max = 15, message = "8자 이상, 15자 이하")
+    @Size(min = 8, max = 15, message = "8자 이상, 15자 이하로 작성해주세요.")
     @Pattern(regexp = "^[A-Za-z0-9`~!@#$%^&*()-_=+]*$", message = "알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자(`~!@#$%^&*()-_=+)")
     private String password;
+
+    @NotBlank
+    @Size(min = 6, max = 100, message = "6자 이상, 100자 이하로 작성해주세요.")
+    private String introduce;
+
+    @NotBlank
+    //이메일 정규 표현식    
+    @Pattern(regexp = "^@Pattern(regexp = \"^[A-Za-z0-9_\\\\.\\\\-]+@[A-Za-z0-9\\\\-]+\\\\.[A-Za-z0-9\\\\-]+$\")", message = "이메일 형식에 맞게 입력해주세요.")
+    private String email;
 }
