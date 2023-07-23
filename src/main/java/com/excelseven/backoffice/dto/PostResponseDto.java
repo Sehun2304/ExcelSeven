988687b4fe7c7ee9b7f6b2ac5d5f1cbd3a94e915
euna2000 <eunakoh2000@gmail.com>
2023-07-23 16:14:36 +0900

    @EnableJpaAuditing 추가, API 수정, equals와 hashCode 메서드 구현

--- src/main/java/com/excelseven/backoffice/dto/PostResponseDto.java
+++ src/main/java/com/excelseven/backoffice/dto/PostResponseDto.java
@@ -4,9 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Objects;
 
-
+@Getter
+@Setter
 public class PostResponseDto {
     private Long postId;
     private int state;
@@ -28,13 +29,30 @@ public class PostResponseDto {
     }
 
     // 사용자 정보를 담는 UserDto 클래스 (내부 클래스)
+    @Getter
+    @Setter
     public static class UserDto {
         private Long id;
         private String username;
-        private String password;
         private String introduction;
         private String image;
 
         // 생성자, 게터, 세터 등 필요한 코드 추가
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            UserDto userDto = (UserDto) o;
+            return Objects.equals(id, userDto.id) &&
+                    Objects.equals(username, userDto.username) &&
+                    Objects.equals(introduction, userDto.introduction) &&
+                    Objects.equals(image, userDto.image);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, username, introduction, image);
+        }
     }
 }
